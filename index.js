2c9971756a02b649926bebdd8aaa42d69a1f31fb
hellnomary <maria.porcires@outlook.com>
2023-07-02 20:33:51 +0200

    cards

---
+++ index.js
@@ -0,0 +1,14 @@
+let firstCard = 2;
+let secondCard = 9;
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log("one more?");
+}
+else if (sum === 21) {
+    console.log("You won!");
+}
+else if (sum > 21) {
+    console.log("You lose")
+}
+
