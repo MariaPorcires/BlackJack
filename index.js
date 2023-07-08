febfada7c3e91385de3d01fee1c956a38955cb4c
hellnomary <maria.porcires@outlook.com>
2023-07-08 14:01:33 +0200

    some css

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-let firstCard = 2;
-let secondCard = 9;
+let firstCard = 6;
+let secondCard = 10;
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-if (sum < 21) {
-    console.log("one more?");
-}
-else if (sum === 21) {
-    console.log("You won!");
-}
-else if (sum > 21) {
-    console.log("You lose")
+if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+} else if (sum === 21) {
+    message = "Blackjack!";
+    hasBlackJack = true
+} else {
+    message = "You lose"
+    isAlive = false
 }
 
+console.log(message);
