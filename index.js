4fcc5a74001a02bfb75635f70928312412469226
hellnomary <maria.porcires@outlook.com>
2023-07-08 17:11:56 +0200

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 11;
-let secondCard = 10;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -9,13 +9,29 @@ let messageEL = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard() {
+
+    let randomNumber = Math.floor( Math.random() * 13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
     
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: " 
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
 
     if (sum <= 20) {
@@ -33,7 +49,7 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 5;
+    let card = getRandomCard();
     sum += card
     cards.push(card)
     console.log(cards);
