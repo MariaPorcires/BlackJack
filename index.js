8d92dc387342f0889123a1c8a6d1c5f16275a9f7
hellnomary <maria.porcires@outlook.com>
2023-07-08 15:19:24 +0200

    added array

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-let firstCard = 6;
+let firstCard = 11;
 let secondCard = 10;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEL = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum
 
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
@@ -16,7 +27,17 @@ function startGame() {
         message = "You lose"
         isAlive = false
     }
-console.log(message);
+
+    messageEL.textContent = message
+   
+}
+
+function newCard() {
+    let card = 5;
+    sum += card
+    cards.push(card)
+    console.log(cards);
+    renderGame()
 }
 
 
